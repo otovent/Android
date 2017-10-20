eaca06c74beb400d25a9d067f265c2485f8bc6b1
igprad <ign.aldi.pradana@gmail.com>
2017-10-20 07:27:41 +0700

    Success to get to endpoint API, make new Class called Service to collect methods to backend

---
+++ app/src/main/java/ws/wolfsoft/creative/API/Impl/ServiceImpl.java
@@ -0,0 +1,58 @@
+package ws.wolfsoft.creative.API.Impl;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import nrew.Otovent.R;
+import ws.wolfsoft.creative.API.Service;
+import ws.wolfsoft.creative.signin;
+
+/**
+ * Created by root on 20/10/17.
+ */
+
+public class ServiceImpl implements Service{
+
+    @Override
+    public void authToBackend(String endpoint, String username, String password, final Context callingClass, final ProgressDialog progressDialog) {
+        RequestQueue queue = Volley.newRequestQueue(callingClass);
+
+        Map<String, String> params = new HashMap<>();
+        params.put("username", username);
+        params.put("password", password);
+
+        JsonObjectRequest requestLogin = new JsonObjectRequest(callingClass.getString(R.string.ENV_HOST_BACKEND) + endpoint,
+                new JSONObject(params), new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                Toast.makeText(callingClass, response.toString(), Toast.LENGTH_SHORT).show();
+                progressDialog.dismiss();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(callingClass, "Tes Sukses", Toast.LENGTH_SHORT).show();
+                Log.i("Result", error.toString());
+                progressDialog.dismiss();
+            }
+        });
+        progressDialog.setIndeterminate(true);
+        progressDialog.setMessage("Please wait");
+        progressDialog.show();
+
+        queue.add(requestLogin);
+    }
+}
